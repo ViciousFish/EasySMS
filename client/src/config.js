b3a49c480540c3c45ea06cf20c7d3ffb2e08eaf6
James Quigley <james.a.quigley@asu.edu>
2018-12-16 20:40:11 -0700

    Fix up local vs prod api urls

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL || 'http://localhost:3000/api'; // eslint-disable-line
-// export const API_URL = 'https://ohack2018-team19.now.sh/api'; // eslint-disable-line
+// export const API_URL = process.env.API_URL || 'http://localhost:3000/api'; // eslint-disable-line
+export const API_URL = 'https://ohack2018-team19.now.sh/api' || process.env.API_URL; // eslint-disable-line
