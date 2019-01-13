cc6a54782206a804bcb9a83a329609f843b50157
James Quigley <james.a.quigley@asu.edu>
2019-01-13 15:04:16 -0700

    Add delete text to the campaign delete button

--- client/src/views/campaign/CampaignMain.vue
+++ client/src/views/campaign/CampaignMain.vue
@@ -8,14 +8,17 @@
       <span class="status-created" v-if="campaign.status==='created'">.</span>
       <span class="status-in-progress" v-if="campaign.status==='in-progress'">.</span>
       {{campaign.name}}
-      <button
-        v-on:click="(e) => {
-          e.preventDefault();
-          deleteCampaign(campaign.id);
-        }"
-        style="background-color:#f64949;float:right;border-radius:50%;border:none;width:25px;height:25px;">
-        X
-      </button>
+      <div style="float:right;">
+        <span style="margin-right:30px">Delete:</span>
+        <button
+          v-on:click="(e) => {
+            e.preventDefault();
+            deleteCampaign(campaign.id);
+          }"
+          style="background-color:#f64949;border-radius:50%;border:none;width:25px;height:25px;">
+          X
+        </button>
+      </div>
     </router-link>
     <router-link class="p3" to="/new">+ new</router-link>
   </div>
